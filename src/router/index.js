160270f3a55e8d08ebb71be52c1d3d6f6ed566ed
chencheng <chenchenxp@126.com>
2021-09-01 10:32:45 +0800

    feat: 增加基础菜单模版

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Layout from '@/components/Layout.vue'
 
 Vue.use(VueRouter);
 const originalPush = VueRouter.prototype.push
@@ -8,7 +9,38 @@ VueRouter.prototype.push = function push(location, onResolve, onReject) {
   return originalPush.call(this, location).catch(err => err)
 }
 
-const routes = []
+export const routes = [{
+  path: '/',
+  code: 'Layout',
+  component: Layout,
+  redirect: '/home',
+  children:[{
+    path: '/home',
+    code: 'Home',
+    component: ()=>import('@/views/Home.vue'),
+    meta: { title: '一级菜单', icon: 'el-icon-setting' },
+    
+  },]
+},{
+  path: '/second',
+  code: 'Layout',
+  component: Layout,
+  meta: { title: '多级菜单', icon: 'el-icon-setting' },
+  children:[{
+    path: '/home1',
+    code: 'Home',
+    component: ()=>import('@/views/Home.vue'),
+    meta: { title: '首页', icon: 'el-icon-setting' },
+    
+  },{
+    path: '/home2',
+    code: 'Home',
+    component: ()=>import('@/views/Home.vue'),
+    meta: { title: '首页', icon: 'el-icon-setting' },
+    
+  },]
+}]
+
 export default new VueRouter({
   routes
 })
